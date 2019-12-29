6bb9a24cbf5ec8d8d9084688d171eaa2b078abb3
Ashutosh Chauhan <masterashu@live.in>
2019-12-29 15:55:16 +0530

    adding dark theme

--- lib/calc.dart
+++ lib/calc.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 import 'custom_button.dart';
 
 // Light Theme Buttons
@@ -33,32 +34,32 @@ MyColorTheme otherButtonLight = MyColorTheme(
 
 // Dark Theme Buttons
 MyColorTheme numberButtonDark = MyColorTheme(
-  Color.fromARGB(255, 238, 238, 238),
   Color.fromARGB(255, 48, 48, 48),
   Color.fromARGB(255, 243, 243, 243),
-  Colors.white70,
-  Color.fromARGB(255, 220, 220, 220),
+  Color.fromARGB(255, 48, 48, 48),
+  Color.fromARGB(255, 52, 52, 52),
+  Color.fromARGB(255, 40, 40, 40),
 );
 MyColorTheme zeroButtonDark = MyColorTheme(
-  Color.fromARGB(255, 238, 238, 238),
   Color.fromARGB(255, 48, 48, 48),
   Color.fromARGB(255, 243, 243, 243),
-  Color.fromARGB(255, 220, 220, 220),
-  Color.fromARGB(255, 255, 255, 255),
+  Color.fromARGB(255, 48, 48, 48),
+  Color.fromARGB(255, 52, 52, 52),
+  Color.fromARGB(255, 40, 40, 40),
 );
 MyColorTheme mathButtonDark = MyColorTheme(
-  Color.fromARGB(255, 238, 238, 238),
+  Color.fromARGB(255, 48, 48, 48),
   Color.fromARGB(255, 238, 134, 47),
   Color.fromARGB(255, 48, 48, 48),
-  Colors.white70,
-  Color.fromARGB(255, 48, 48, 48),
+  Color.fromARGB(255, 52, 52, 52),
+  Color.fromARGB(255, 40, 40, 40),
 );
 MyColorTheme otherButtonDark = MyColorTheme(
-  Color.fromARGB(255, 238, 238, 238),
-  Color.fromARGB(255, 238, 134, 47),
-  Color.fromARGB(255, 243, 243, 243),
-  Colors.white70,
+  Color.fromARGB(255, 48, 48, 48),
   Color.fromARGB(255, 166, 166, 166),
+  Color.fromARGB(255, 48, 48, 48),
+  Color.fromARGB(255, 52, 52, 52),
+  Color.fromARGB(255, 40, 40, 40),
 );
 
 class Home extends StatefulWidget {
@@ -67,21 +68,57 @@ class Home extends StatefulWidget {
 }
 
 class _HomeState extends State<Home> {
+  bool isDark;
+
+  _loadTheme() async {
+    SharedPreferences prefs = await SharedPreferences.getInstance();
+    setState(() {
+      isDark = prefs.getBool('isDark');
+    });
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    isDark = false;
+    _loadTheme();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       body: Container(
-        color: Color.fromARGB(255, 238, 238, 238),
+        color: (isDark == false)
+            ? Color.fromARGB(255, 238, 238, 238)
+            : Color.fromARGB(255, 48, 48, 48),
         child: Column(
           mainAxisAlignment: MainAxisAlignment.spaceBetween,
           children: <Widget>[
-            Padding(
-              padding: EdgeInsets.only(top: 96, right: 10),
-              child: Row(
-                mainAxisAlignment: MainAxisAlignment.end,
-                children: <Widget>[
-                  Text("3,780", style: TextStyle(fontSize: 96, fontFamily: "Montserrat"),),
-                ],
+            GestureDetector(
+              onLongPress: () async {
+                setState(() {
+                  isDark = !isDark;
+                });
+                SharedPreferences prefs = await SharedPreferences.getInstance();
+                await prefs.setBool('isDark', isDark);
+              },
+              child: Padding(
+                padding: EdgeInsets.only(top: 96, right: 10),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.end,
+                  children: <Widget>[
+                    Text(
+                      "3,780",
+                      style: TextStyle(
+                        fontSize: 96,
+                        fontFamily: "Montserrat",
+                        color: (isDark == false)
+                            ? Color.fromARGB(255, 48, 48, 48)
+                            : Color.fromARGB(255, 238, 238, 238),
+                      ),
+                    ),
+                  ],
+                ),
               ),
             ),
             Padding(
@@ -89,149 +126,145 @@ class _HomeState extends State<Home> {
               child: Column(
                 mainAxisSize: MainAxisSize.max,
                 children: <Widget>[
-                  Row(
-                    mainAxisSize: MainAxisSize.max,
-                    mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                    children: <Widget>[
-                      MyButton(
-                          "AC",
-                          (MediaQuery.of(context).platformBrightness ==
-                                  Brightness.light)
-                              ? otherButtonLight
-                              : otherButtonDark),
-                      MyButton(
-                          "+/-",
-                          (MediaQuery.of(context).platformBrightness ==
-                                  Brightness.light)
-                              ? otherButtonLight
-                              : otherButtonDark),
-                      MyButton(
-                          "%",
-                          (MediaQuery.of(context).platformBrightness ==
-                                  Brightness.light)
-                              ? otherButtonLight
-                              : otherButtonDark),
-                      MyButton(
-                          "÷",
-                          (MediaQuery.of(context).platformBrightness ==
-                                  Brightness.light)
-                              ? mathButtonLight
-                              : mathButtonDark),
-                    ],
+                  Padding(
+                    padding: EdgeInsets.only(top: 8),
+                    child: Row(
+                      mainAxisSize: MainAxisSize.max,
+                      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                      children: <Widget>[
+                        MyButton(
+                            "AC",
+                            (isDark == false)
+                                ? otherButtonLight
+                                : otherButtonDark),
+                        MyButton(
+                            "+/-",
+                            (isDark == false)
+                                ? otherButtonLight
+                                : otherButtonDark),
+                        MyButton(
+                            "%",
+                            (isDark == false)
+                                ? otherButtonLight
+                                : otherButtonDark),
+                        MyButton(
+                            "÷",
+                            (isDark == false)
+                                ? mathButtonLight
+                                : mathButtonDark),
+                      ],
+                    ),
                   ),
-                  Row(
-                    mainAxisSize: MainAxisSize.max,
-                    mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                    children: <Widget>[
-                      MyButton(
-                          "7",
-                          (MediaQuery.of(context).platformBrightness ==
-                                  Brightness.light)
-                              ? numberButtonLight
-                              : numberButtonDark),
-                      MyButton(
-                          "8",
-                          (MediaQuery.of(context).platformBrightness ==
-                                  Brightness.light)
-                              ? numberButtonLight
-                              : numberButtonDark),
-                      MyButton(
-                          "9",
-                          (MediaQuery.of(context).platformBrightness ==
-                                  Brightness.light)
-                              ? numberButtonLight
-                              : numberButtonDark),
-                      MyButton(
-                          "×",
-                          (MediaQuery.of(context).platformBrightness ==
-                                  Brightness.light)
-                              ? mathButtonLight
-                              : mathButtonDark),
-                    ],
+                  Padding(
+                    padding: EdgeInsets.only(top: 8),
+                    child: Row(
+                      mainAxisSize: MainAxisSize.max,
+                      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                      children: <Widget>[
+                        MyButton(
+                            "7",
+                            (isDark == false)
+                                ? numberButtonLight
+                                : numberButtonDark),
+                        MyButton(
+                            "8",
+                            (isDark == false)
+                                ? numberButtonLight
+                                : numberButtonDark),
+                        MyButton(
+                            "9",
+                            (isDark == false)
+                                ? numberButtonLight
+                                : numberButtonDark),
+                        MyButton(
+                            "×",
+                            (isDark == false)
+                                ? mathButtonLight
+                                : mathButtonDark),
+                      ],
+                    ),
                   ),
-                  Row(
-                    mainAxisSize: MainAxisSize.max,
-                    mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                    children: <Widget>[
-                      MyButton(
-                          "4",
-                          (MediaQuery.of(context).platformBrightness ==
-                                  Brightness.light)
-                              ? numberButtonLight
-                              : numberButtonDark),
-                      MyButton(
-                          "5",
-                          (MediaQuery.of(context).platformBrightness ==
-                                  Brightness.light)
-                              ? numberButtonLight
-                              : numberButtonDark),
-                      MyButton(
-                          "6",
-                          (MediaQuery.of(context).platformBrightness ==
-                                  Brightness.light)
-                              ? numberButtonLight
-                              : numberButtonDark),
-                      MyButton(
-                          "−",
-                          (MediaQuery.of(context).platformBrightness ==
-                                  Brightness.light)
-                              ? mathButtonLight
-                              : mathButtonDark),
-                    ],
+                  Padding(
+                    padding: EdgeInsets.only(top: 8),
+                    child: Row(
+                      mainAxisSize: MainAxisSize.max,
+                      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                      children: <Widget>[
+                        MyButton(
+                            "4",
+                            (isDark == false)
+                                ? numberButtonLight
+                                : numberButtonDark),
+                        MyButton(
+                            "5",
+                            (isDark == false)
+                                ? numberButtonLight
+                                : numberButtonDark),
+                        MyButton(
+                            "6",
+                            (isDark == false)
+                                ? numberButtonLight
+                                : numberButtonDark),
+                        MyButton(
+                            "−",
+                            (isDark == false)
+                                ? mathButtonLight
+                                : mathButtonDark),
+                      ],
+                    ),
                   ),
-                  Row(
-                    mainAxisSize: MainAxisSize.max,
-                    mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                    children: <Widget>[
-                      MyButton(
-                          "1",
-                          (MediaQuery.of(context).platformBrightness ==
-                                  Brightness.light)
-                              ? numberButtonLight
-                              : numberButtonDark),
-                      MyButton(
-                          "2",
-                          (MediaQuery.of(context).platformBrightness ==
-                                  Brightness.light)
-                              ? numberButtonLight
-                              : numberButtonDark),
-                      MyButton(
-                          "3",
-                          (MediaQuery.of(context).platformBrightness ==
-                                  Brightness.light)
-                              ? numberButtonLight
-                              : numberButtonDark),
-                      MyButton(
-                          "+",
-                          (MediaQuery.of(context).platformBrightness ==
-                                  Brightness.light)
-                              ? mathButtonLight
-                              : mathButtonDark),
-                    ],
+                  Padding(
+                    padding: EdgeInsets.only(top: 8),
+                    child: Row(
+                      mainAxisSize: MainAxisSize.max,
+                      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                      children: <Widget>[
+                        MyButton(
+                            "1",
+                            (isDark == false)
+                                ? numberButtonLight
+                                : numberButtonDark),
+                        MyButton(
+                            "2",
+                            (isDark == false)
+                                ? numberButtonLight
+                                : numberButtonDark),
+                        MyButton(
+                            "3",
+                            (isDark == false)
+                                ? numberButtonLight
+                                : numberButtonDark),
+                        MyButton(
+                            "+",
+                            (isDark == false)
+                                ? mathButtonLight
+                                : mathButtonDark),
+                      ],
+                    ),
                   ),
-                  Row(
-                    mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                    mainAxisSize: MainAxisSize.max,
-                    children: <Widget>[
-                      MyLongButton(
-                          "0",
-                          (MediaQuery.of(context).platformBrightness ==
-                                  Brightness.light)
-                              ? zeroButtonLight
-                              : zeroButtonDark),
-                      MyButton(
-                          ".",
-                          (MediaQuery.of(context).platformBrightness ==
-                                  Brightness.light)
-                              ? numberButtonLight
-                              : numberButtonDark),
-                      MyButton(
-                          "=",
-                          (MediaQuery.of(context).platformBrightness ==
-                                  Brightness.light)
-                              ? mathButtonLight
-                              : mathButtonDark),
-                    ],
+                  Padding(
+                    padding: EdgeInsets.only(top: 8),
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                      mainAxisSize: MainAxisSize.max,
+                      children: <Widget>[
+                        MyLongButton(
+                            "0",
+                            (isDark == false)
+                                ? zeroButtonLight
+                                : zeroButtonDark),
+                        MyButton(
+                            ".",
+                            (isDark == false)
+                                ? numberButtonLight
+                                : numberButtonDark),
+                        MyButton(
+                            "=",
+                            (isDark == false)
+                                ? mathButtonLight
+                                : mathButtonDark),
+                      ],
+                    ),
                   ),
                 ],
               ),
--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:shared_preferences/shared_preferences.dart';
 import 'calc.dart';
 
 void main()  {
